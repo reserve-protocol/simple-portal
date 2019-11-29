b45a202b7fea1d47712b59c0507241a1a5cd410d
Taylor Brent <taylor.w.brent@gmail.com>
2019-11-29 12:47:51 -0800

    resolve merge

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -3,12 +3,18 @@ import SimpleStorage from "./contracts/SimpleStorage.json";
 import ComplexStorage from "./contracts/ComplexStorage.json";
 import TutorialToken from "./contracts/TutorialToken.json";
 
+import USDC from "./contracts/USDC.json";
+import TUSD from "./contracts/TUSD.json";
+import PAX from "./contracts/PAX.json";
+import Reserve from "./contracts/Reserve.json";
+import Manager from "./contracts/Manager.json";
+
 const options = {
   web3: {
     block: false,
     customProvider: new Web3("ws://localhost:8545"),
   },
-  contracts: [SimpleStorage, ComplexStorage, TutorialToken],
+  contracts: [SimpleStorage, ComplexStorage, TutorialToken, USDC, TUSD, PAX, Reserve, Manager],
   events: {
     SimpleStorage: ["StorageSet"],
   },
